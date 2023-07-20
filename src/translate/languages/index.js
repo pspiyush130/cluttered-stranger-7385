cb9d5a25e7b827b31d6163636aa382b94ad7307d
Piyush Sharma <118152296+pspiyush130@users.noreply.github.com>
2023-07-21 00:51:53 +0530

    clean

--- src/translate/languages/index.js
+++
@@ -1,9 +0,0 @@
-import { StringsComuns as portugueseMessages } from './pt'
-import { StringsComuns as englishMessages } from './en'
-
-const StringsComuns = {
-    ...portugueseMessages,
-    ...englishMessages
-}
-
-export { StringsComuns }
